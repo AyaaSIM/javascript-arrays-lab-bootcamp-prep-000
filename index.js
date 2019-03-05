12aa039036d86c63dd237aab2648e58cf4d595fd
Ayaa <ayaa1siddig5@gmail.com>
2019-03-05 09:12:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyRemoveFirstKitten(){
   kittens.shift()
 }
 function appendKitten(name){
-   kittens = kittens = [...kittens, name];
+  var kittens = [...kittens, name];
    return kittens
   }
